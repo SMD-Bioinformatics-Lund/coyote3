a951f3e82c8894510341cf96606282669aac67a1
sailepradh <sailepradh@gmail.com>
2025-04-22 17:27:59 +0200

    adding description with the extenable features

--- coyote/blueprints/rna/filters.py
+++ coyote/blueprints/rna/filters.py
@@ -1,9 +1,56 @@
 from flask import current_app as app
 import os
 from urllib.parse import unquote
-from markupsafe import escape
+from markupsafe import Markup, escape
 import arrow
 import dateutil
+
+
+@app.template_filter()
+def format_fusion_desc_few(st, preview_count=None):
+    if not st:
+        return ""
+
+    good_terms = set([
+        "mitelman", "18cancers", "known", "oncogene", "cgp", "cancer", "cosmic", "gliomas", 
+        "oesophagus", "tumor", "pancreases", "prostates", "tcga", "ticdb", "high",
+    ])
+    verybad_terms = set([
+        "1000genomes", "banned", "bodymap2", "cacg", "conjoing", "cortex", "cta", "ctb", 
+        "ctc", "ctd", "distance1000bp", "ensembl_fully_overlapping", "ensembl_same_strand_overlapping",
+        "gtex", "hpa", "matched-normal", "mt", "non_cancer_tissues", "non_tumor_cells",
+        "pair_pseudo_genes", "paralogs", "readthrough", "refseq_fully_overlapping", "rp11", "rp", 
+        "rrna", "similar_reads", "similar_symbols", "ucsc_fully_overlapping", "ucsc_same_strand_overlapping",
+    ])
+    bad_terms = set([
+        "distance100kbp", "distance10kbp", "duplicates", "ensembl_partially_overlapping", "fragments",
+        "healthy", "short_repeats", "long_repeats", "partial-matched-normal",
+        "refseq_partially_overlapping", "short_distance", "ucsc_partially_overlapping",
+    ])
+
+    term_to_class = {
+        **{term: "bg-green-500 text-white" for term in good_terms},
+        **{term: "bg-red-500 text-white" for term in verybad_terms},
+        **{term: "bg-pink-500 text-white" for term in bad_terms},
+    }
+
+    terms = st.split(",")
+
+    if preview_count is not None:
+        terms = terms[:preview_count]
+
+    html_parts = []
+    for i, v in enumerate(terms):
+        v_clean = v.strip()
+        term_class = term_to_class.get(v_clean, "bg-gray-500 text-white")
+        html_parts.append(
+            f"<span class='inline-block px-2 py-1 text-xs font-semibold {term_class} rounded-full mr-1 mb-1'>{escape(v_clean)}</span>"
+        )
+        if (i + 1) % 3 == 0 and (i + 1) != len(terms):
+            html_parts.append("<br>")
+
+    return Markup("".join(html_parts))
+
 
 
 @app.template_filter()
